83e6348366c14ecd53d25b5b0db27cd76e72c457
EljerPenny <elijahjpenny@gmail.com>
2023-03-13 12:08:35 -0400

    Can CRUD toys and give and take them from finches

--- main_app/models.py
+++ main_app/models.py
@@ -8,13 +8,24 @@ MEALS = (
 )
 
 # Create your models here.
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.color} {self.name}'
+    
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
+
 class Finch(models.Model):
     name= models.CharField(max_length=100)
     species = models.CharField(max_length=100)
     color = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
-
+    toys = models.ManyToManyField(Toy)
+    
     def __str__(self):
         return f"Name: {self.name}, {self.species}"
     def get_absolute_url(self):
